780631019aa88020a8dd9869fe69af7390a58ac7
bryceandy <bryceandy@rocketmail.com>
2019-12-25 02:06:23 +0300

    First prototype, download meme and save to gallery

---
+++ src/js/app.js
@@ -0,0 +1,59 @@
+import $$ from 'dom7';
+import Framework7 from 'framework7/framework7.esm.bundle.js';
+
+// Import F7 Styles
+import 'framework7/css/framework7.bundle.css';
+
+// Import Icons and App Custom Styles
+import '../css/icons.css';
+import '../css/app.scss';
+
+// Import Cordova APIs
+import cordovaApp from './cordova-app.js';
+
+// Import Routes
+import routes from './routes.js';
+
+var app = new Framework7({
+  root: '#app', // App root element
+  id: 'com.bryce.memely', // App bundle ID
+  name: 'memely', // App name
+  theme: 'auto', // Automatic theme detection
+
+  // App root data
+  data: function () {
+    return {
+      XRapidAPIKey: '5a5db81725mshedb8ee9d33ab24fp163f56jsnc051c51e6ced',
+      XRapidAPIHost: 'ronreiter-meme-generator.p.rapidapi.com',
+      testEndpoint: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlOzdvR4MOcZAtBUuY_tURF82DH3dbSAuOa_GEv0lf6ItPxCNPBA&s'
+    };
+  },
+  // App root methods
+  methods: {
+
+  },
+  // App routes
+  routes: routes,
+
+  // Input settings
+  input: {
+    scrollIntoViewOnFocus: Framework7.device.cordova && !Framework7.device.electron,
+    scrollIntoViewCentered: Framework7.device.cordova && !Framework7.device.electron,
+  },
+  // Cordova Statusbar settings
+  statusbar: {
+    iosOverlaysWebView: false,
+    androidOverlaysWebView: false,
+    androidBackgroundColor: "#ffffff"
+  },
+  on: {
+    init: function () {
+      var f7 = this;
+      if (f7.device.cordova) {
+        // Init cordova APIs (see cordova-app.js)
+        cordovaApp.init(f7);
+      }
+    },
+  },
+});
+
---
+++ src/js/routes.js
@@ -0,0 +1,21 @@
+
+import HomePage from '../pages/home.f7.html';
+import CatalogPage from '../pages/catalog.f7.html';
+import SettingsPage from '../pages/settings.f7.html';
+
+var routes = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/catalog/',
+    component: CatalogPage,
+  },
+  {
+    path: '/settings/',
+    component: SettingsPage,
+  },
+];
+
+export default routes;
